cdeb46436881647e6c80a8a8d077958a8ff3d71d
Saska Dönges <saskeli@kapsi.fi>
2023-06-28 10:49:37 +0300

    I am hello!

--- mpi/hello-world/hello.cpp
+++ mpi/hello-world/hello.cpp
@@ -1,7 +1,28 @@
 #include <iostream>
+#include <mpi.h>
+#include <cstdlib>
 
 int main(int argc, char *argv[])
 {
-    // TODO: say hello! in parallel
-    std::cout << "Hello!" << std::endl;
+    int rc = MPI_Init(&argc, &argv);
+    int rank, size;
+    rc |= MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    rc |= MPI_Comm_size(MPI_COMM_WORLD, &size);
+    if (rc != MPI_SUCCESS) {
+        std::cerr << "Failed to init MPI" << std::endl;
+        exit(1);
+    }
+    
+
+    char* c = (char*)malloc(MPI_MAX_PROCESSOR_NAME);
+    int len;
+    MPI_Get_processor_name(c, &len);
+    
+
+    std::cout << "Hello, I am " << rank << "! Running on " << c << std::endl;
+    if (rank == 0) {
+        std::cout << "There are a total of " << size << " task" << std::endl;
+    }
+
+    MPI_Finalize();
 }
